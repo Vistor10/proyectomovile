8cb09a059185339501855e63b22f72246b00ec58
Vistor10 <vi.arroyo@duocuc.cl>
2024-08-29 18:36:52 -0400

    validacion hecha en pagina recuperar contraseña

--- src/app/pages/recuperarcontrasena/recuperarcontrasena.page.ts
+++ src/app/pages/recuperarcontrasena/recuperarcontrasena.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperarcontrasena',
@@ -8,11 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class RecuperarcontrasenaPage implements OnInit {
   email: string = '';
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
   onForward() {
+    console.log('Correo válido:', this.email);
+    this.navCtrl.navigateRoot('/login');
 
   }
 
