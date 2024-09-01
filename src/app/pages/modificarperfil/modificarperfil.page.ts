5300af5480d4645fb2bbad776031ee4102dd5f86
Vistor10 <vi.arroyo@duocuc.cl>
2024-08-31 22:07:48 -0400

    pagina de modificarperfil terminada

--- src/app/pages/modificarperfil/modificarperfil.page.ts
+++ src/app/pages/modificarperfil/modificarperfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modificarperfil',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificarperfil.page.scss'],
 })
 export class ModificarperfilPage implements OnInit {
+  newUsername: string = '';
+  newEmail: string = '';
+  newPassword: string = '';
+  confirmNewPassword: string = '';
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
+  passwordsMatch(): boolean {
+    return this.newPassword === this.confirmNewPassword;
+  }
 
+  onUpdateProfile() {
+    if (this.passwordsMatch() && this.newPassword.length >= 8) {
+      console.log('Perfil actualizado:', {
+        username: this.newUsername,
+        email: this.newEmail,
+        password: this.newPassword,
+      });
+      this.navCtrl.navigateRoot('/perfil');
+    } else {
+      console.log('Las contraseñas no coinciden o no cumplen con los requisitos.');
+    }
+  }
 }
