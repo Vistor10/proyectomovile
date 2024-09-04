bcba713af8e6b5ed40316ddeb0efcb3c935b38ea
Vistor10 <vi.arroyo@duocuc.cl>
2024-09-04 19:13:45 -0400

    validaciones hechas en eliminar producto

--- src/app/pages/eliminarproducto/eliminarproducto.page.ts
+++ src/app/pages/eliminarproducto/eliminarproducto.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { NavController, ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-eliminarproducto',
   templateUrl: './eliminarproducto.page.html',
   styleUrls: ['./eliminarproducto.page.scss'],
 })
 export class EliminarproductoPage implements OnInit {
-
-  constructor() { }
+  productoSeleccionado: string = '';
+  constructor(private navCtrl: NavController, private toastController: ToastController) { }
 
   ngOnInit() {
+  }
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+    });
+    toast.present();
+  }
+  async onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Producto eliminado:', this.productoSeleccionado);
+      this.presentToast('Producto eliminado exitosamente.');
+      this.navCtrl.navigateRoot('/perfil');
+    } else {
+      this.presentToast('Por favor, selecciona un producto para eliminar.');
+    }
   }
 
 }
