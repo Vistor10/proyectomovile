ee40c1da1558aa493d39b678bf5eb5bfe644a082
Vistor10 <vi.arroyo@duocuc.cl>
2024-09-09 01:29:01 -0300

    pagina de compra finalizada y agrego de ruta en carrito de compras

--- src/app/pages/compra/compra.page.ts
+++ src/app/pages/compra/compra.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-compra',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compra.page.scss'],
 })
 export class CompraPage implements OnInit {
-
-  constructor() { }
+  recipientName: string = '';
+  address: string = '';
+  city: string = '';
+  constructor(private navCtrl: NavController, private toastController: ToastController) { }
 
   ngOnInit() {
   }
+  async onSubmit() {
+    const toast = await this.toastController.create({
+      message: 'Compra confirmada. Enviando a: ' + this.address,
+      duration: 5000,
+      position: 'bottom',
+    });
+    toast.present();
 
+    this.navCtrl.navigateRoot('/paginainicio'); 
+  }
 }
+
