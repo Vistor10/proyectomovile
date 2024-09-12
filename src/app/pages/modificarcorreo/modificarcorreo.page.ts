54bfcd3c0f55dd897ec5c14fb1d6d25a79f77457
Vistor10 <vi.arroyo@duocuc.cl>
2024-09-12 20:03:18 -0300

    pagina de modificar correo terminada

--- src/app/pages/modificarcorreo/modificarcorreo.page.ts
+++ src/app/pages/modificarcorreo/modificarcorreo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modificarcorreo',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificarcorreo.page.scss'],
 })
 export class ModificarcorreoPage implements OnInit {
-
-  constructor() { }
+  newEmail: string = '';
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
-
+  onUpdateEmail() {
+    if (this.newEmail.trim() !== '' && this.newEmail.includes('@')) {
+      console.log('Correo actualizado:', this.newEmail);
+      this.navCtrl.navigateRoot('/perfil'); 
+    } else {
+      console.log('Error en el correo.');
+    }
+  }
 }
