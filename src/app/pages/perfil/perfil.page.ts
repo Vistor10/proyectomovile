f321d4237ac6db03df9514d50cfc4ae0ac8d4eec
Vistor10 <vi.arroyo@duocuc.cl>
2024-10-07 23:04:22 -0300

    camara

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Camera, CameraResultType } from '@capacitor/camera';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
@@ -7,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
   isAdmin: boolean = false;
+  imagen: any;
   constructor() { }
 
+  
   ngOnInit() {
     const adminStatus = localStorage.getItem('isAdmin');
     this.isAdmin = adminStatus === 'true';
   }
+  takePicture = async () => {
+    const takePicture =  async () =>{
+      const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri
+    });
+  
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    this.imagen = image.webPath;
+  
+    // Can be set to the src of an image now
+
+  };
 
 }
+}
+
