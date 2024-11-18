099d908897ead35570febbae51916c0f5317aee4
Vistor10 <vi.arroyo@duocuc.cl>
2024-11-18 19:29:50 -0300

    creacion de pagina para recuperar la contraseña olvidada

---
+++ src/app/pages/contrasenanuevaolvido/contrasenanuevaolvido.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ContrasenanuevaolvidoPage } from './contrasenanuevaolvido.page';
+
+describe('ContrasenanuevaolvidoPage', () => {
+  let component: ContrasenanuevaolvidoPage;
+  let fixture: ComponentFixture<ContrasenanuevaolvidoPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContrasenanuevaolvidoPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/contrasenanuevaolvido/contrasenanuevaolvido.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contrasenanuevaolvido',
+  templateUrl: './contrasenanuevaolvido.page.html',
+  styleUrls: ['./contrasenanuevaolvido.page.scss'],
+})
+export class ContrasenanuevaolvidoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
