2002c1d6086693b0a848073fd5f1959d4ec8d171
Vistor10 <vi.arroyo@duocuc.cl>
2024-11-18 23:58:59 -0300

    arreglo de spects en historial admin

--- src/app/pages/historialadmin/historialadmin.page.spec.ts
+++ src/app/pages/historialadmin/historialadmin.page.spec.ts
@@ -1,11 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HistorialadminPage } from './historialadmin.page';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { HttpClient } from '@angular/common/http';
 
 describe('HistorialadminPage', () => {
   let component: HistorialadminPage;
   let fixture: ComponentFixture<HistorialadminPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HistorialadminPage],
+      imports: [IonicModule.forRoot(),FormsModule],
+      providers:[SQLite, HttpClient]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HistorialadminPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
